edca88b12cdc37ee0f08adce32e4c3c17f84e7c8
Your Name <you@example.com>
2024-08-11 14:41:06 +0500

    fix

--- src/components/widgets/Navbar/index.jsx
+++ src/components/widgets/Navbar/index.jsx
@@ -1,27 +1,33 @@
 import s from './style.module.scss';
 
-const Navbar = () => {
-  return (
-    <div className={s.nav}>
-      <div className={s.container}>
-        <p className={s.logo}><a href="#header">Вагапов</a></p>
-        <div className={s.rightText}>
-          <ul>
-            <li><a href="#services">Услуги</a></li>
-            <li><a href="#">Сертификаты</a></li>
-            <li><a href="#contacts">Контакты</a></li>
-          </ul>
-          <div className={s.contactsBlock}>
-            <p>Время работы с 8:00 до 18:00</p>
-            <div className={s.phone}>
-              <p><a href="tel:+79044539558">+7 904 453-95-58</a></p>
-              <p><a href="tel:+79581663176">+7 958 166-31-76</a></p>
-            </div>
+const Navbar = () => (
+  <div className={s.wrapper}>
+    <div className={s.content}>
+      <p className={s.logo}>
+        <a href="#header">{'Вагапов'}</a>
+      </p>
+      <div className={s.info}>
+        <ul className={s.navbar}>
+          <li className={s.navItem}>
+            <a href="#services">{'Услуги'}</a>
+          </li>
+          <li className={s.navItem}>
+            <a href="#">{'Сертификаты'}</a>
+          </li>
+          <li className={s.navItem}>
+            <a href="#contacts">{'Контакты'}</a>
+          </li>
+        </ul>
+        <div className={s.contact}>
+          <p className={s.workTime}>{'Время работы с 8:00 до 18:00'}</p>
+          <div className={s.phones}>
+            <a href="tel:+79044539558">{'+7 904 453-95-58'}</a>
+            <a href="tel:+79581663176">{'+7 958 166-31-76'}</a>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
