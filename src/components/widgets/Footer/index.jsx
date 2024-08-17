eec54ff28a7cccf9aa327312b724602eb174688a
Floki-gig <alexeyorekhov2005@mail.ru>
2024-08-17 14:08:40 +0500

    fix fix fix

--- src/components/widgets/Footer/index.jsx
+++ src/components/widgets/Footer/index.jsx
@@ -10,27 +10,29 @@ const Footer = () => (
         />
       </div>
       <div className={s.contacts} id='contacts'>
-        <h1>{'Контакты'}</h1>
-        <div className={s.grid}>
-          <div className={s.gridItem}>
-            <h3>{'График работы'}</h3>
-            <p>{'пн-сб: с 8:00 до 18:00'}</p>
-          </div>
-          <div className={s.gridItem}>
-            <h3>{'Рабочий телефон'}</h3>
-            <p><a href="tel:79044539558">{'+7 904 453-95-58'}</a></p>
-            <p><a href="tel:79581663176">{'+7 958 166-31-76'}</a></p>
-          </div>
-          <div className={s.gridItem}>
-            <h3>{'Почта'}</h3>
-            <p className={s.accent}><a href="mailto:socrat-89@yandex.ru">{'socrat-89@yandex.ru'}</a></p>
-          </div>
-          <div className={s.gridItem}>
-            <h3>{'Адрес'}</h3>
-            <p>{'629000 г.Салехард,'}</p>
-            <p>{'ул. Свердлова, дом 22, офис 214'}</p>
-          </div>
+        <div className={s.contactsBlock}>
+          <h1>{'Контакты'}</h1>
+          <div className={s.grid}>
+            <div className={s.gridItem}>
+              <h3>{'График работы'}</h3>
+              <p>{'пн-сб: с 8:00 до 18:00'}</p>
+            </div>
+            <div className={s.gridItem}>
+              <h3>{'Рабочий телефон'}</h3>
+              <p><a href="tel:79044539558">{'+7 904 453-95-58'}</a></p>
+              <p><a href="tel:79581663176">{'+7 958 166-31-76'}</a></p>
+            </div>
+            <div className={s.gridItem}>
+              <h3>{'Почта'}</h3>
+              <p className={s.accent}><a href="mailto:socrat-89@yandex.ru">{'socrat-89@yandex.ru'}</a></p>
+            </div>
+            <div className={s.gridItem}>
+              <h3>{'Адрес'}</h3>
+              <p>{'629000 г.Салехард,'}</p>
+              <p>{'ул. Свердлова, дом 22, офис 214'}</p>
+            </div>
         </div>
+          </div>
         <div className={s.ap}>
           <p>© Адвокат Вагапов Ришат Ринатович, регистрационный номер 89/390 <a href="https://www.advokatyamal.ru/about/lawyers/vagapov_rishat_rinatovich/" target='__blank'>в реестре адвокатской палаты Ямало-Ненецкого автономного округа</a>. Все права защищены.</p>
         </div>
--- src/components/widgets/Form/index.jsx
+++ src/components/widgets/Form/index.jsx
@@ -3,7 +3,7 @@ import s from './style.module.scss';
 const Form = () => {
   
   function noDigits(event) {
-    if ("1234567890".indexOf(event.key) != -1)
+    if ('1234567890'.indexOf(event.key) != -1)
       event.preventDefault();
   }
   
@@ -19,14 +19,15 @@ const Form = () => {
             <div className={s.text}>
               <p>Проведу вам экспертную консультацию, вникну в суть вашей проблемы и подготовлю оптимальное решение поставленной задачи</p>
             </div>
-            <div className={s.flexForm}>
+            <form className={s.flexForm}>
               <input type="text"  placeholder='Ваше имя' maxLength="45" onKeyDown={noDigits}/>
               <input type="number" inputMode="tel" placeholder='+7 (000) 000-0000' maxLength="11"/>
-              <input type="text" inputMode="email" placeholder='E-mail' />
-              <button className={s.btn}><a href="">Оставить заявку</a></button>
-            </div>        
+              <input type="email"  inputMode="email" placeholder='E-mail' />
+              <input type='submit' value="Отправить заявку" className={s.btn} />
+              <textarea type="text" placeholder='Напишите тему заявки и опишите суть проблемы' className={s.formText}/>
+            </form>        
           </div>
-        </div>
+        </div> 
       </div>
       
     </>
