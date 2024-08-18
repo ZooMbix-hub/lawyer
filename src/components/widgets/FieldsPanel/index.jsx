c3227d475082e57190d67e4e8be708aeeade4f12
Your Name <you@example.com>
2024-08-19 00:01:38 +0500

    add fields

--- src/components/widgets/FieldsPanel/index.jsx
+++ src/components/widgets/FieldsPanel/index.jsx
@@ -1,10 +1,56 @@
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 import {AppContext} from '@app/AppStore';
 import s from './style.module.scss';
 import {Link} from 'react-router-dom';
 
 const FieldsPanel = () => {
-  const {setWorkTime, setNumberPhone, setEmail, setAddress} = useContext(AppContext);
+  const {
+    workTime, 
+    setWorkTime, 
+    numberPhone, 
+    setNumberPhone, 
+    email, 
+    setEmail, 
+    address, 
+    setAddress
+  } = useContext(AppContext);
+
+  const [newFromWorkTime, setNewFromWorkTime] = useState(workTime.from);
+  const [newToWorkTime, setNewToWorkTime] = useState(workTime.to);
+
+  const [newNumberPhoneOne, setNewNumberPhoneOne] = useState(numberPhone[0]);
+  const [newNumberPhoneTwo, setNewNumberPhoneTwo] = useState(numberPhone[1]);
+
+  const [newEmail, setNewEmail] = useState(email);
+
+  const [newIndex, setNewIndex] = useState(address.index);
+  const [newCity, setNewCity] = useState(address.city);
+  const [newStreet, setNewStreet] = useState(address.street);
+
+  const saveData = () => {
+    if (newFromWorkTime || newToWorkTime) {
+      setWorkTime({
+        from: newFromWorkTime,
+        to: newToWorkTime
+      });
+    }
+
+    if (newNumberPhoneOne || newNumberPhoneTwo) {
+      setNumberPhone([newNumberPhoneOne, newNumberPhoneTwo]);
+    }
+
+    if (newEmail) {
+      setEmail(newEmail);
+    }
+
+    if (newIndex || newCity || newStreet) {
+      setAddress({
+        index: newIndex,
+        city: newCity,
+        street: newStreet
+      });
+    }
+  };
 
   return (
     <div className={s.wrapper}>
@@ -17,21 +63,71 @@ const FieldsPanel = () => {
       <div className={s.fieldsContainer}>
         <div className={s.field}>
           <p className={s.titleField}>{'Время работы:'}</p>
-          <input type='text' className={s.textField} />
+          <input 
+            type={'text'} 
+            value={newFromWorkTime} 
+            onChange={({target}) => setNewFromWorkTime(target.value)}
+            className={s.textField} 
+          />
+          <input 
+            type={'text'} 
+            value={newToWorkTime} 
+            onChange={({target}) => setNewToWorkTime(target.value)}
+            className={s.textField} 
+          />
         </div>
         <div className={s.field}>
           <p className={s.titleField}>{'Номера телефона:'}</p>
-          <input type='text' className={s.textField} />
+          <input 
+            type={'text'} 
+            placeholder={'Введите номер'}
+            value={newNumberPhoneOne} 
+            onChange={({target}) => setNewNumberPhoneOne(target.value)}
+            className={s.textField} 
+          />
+          <input 
+            type={'text'} 
+            placeholder={'Введите номер'}
+            value={newNumberPhoneTwo} 
+            onChange={({target}) => setNewNumberPhoneTwo(target.value)}
+            className={s.textField} 
+          />
         </div>
         <div className={s.field}>
           <p className={s.titleField}>{'Почта:'}</p>
-          <input type='text' className={s.textField} />
+          <input 
+            type={'text' }
+            placeholder={'Введите почту'}
+            value={newEmail} 
+            onChange={({target}) => setNewEmail(target.value)}
+            className={s.textField} 
+          />
         </div>
         <div className={s.field}>
           <p className={s.titleField}>{'Адрес:'}</p>
-          <input type='text' className={s.textField} />
+          <input 
+            type={'text'} 
+            placeholder={'Введите индекс города'}
+            value={newIndex} 
+            onChange={({target}) => setNewIndex(target.value)}
+            className={s.textField} 
+          />
+          <input 
+            type={'text'}
+            placeholder={'Введите город'} 
+            value={newCity} 
+            onChange={({target}) => setNewCity(target.value)}
+            className={s.textField} 
+          />
+          <input 
+            type={'text'}
+            placeholder={'Введите улицу'} 
+            value={newStreet} 
+            onChange={({target}) => setNewStreet(target.value)}
+            className={s.textField} 
+          />
         </div>
-        <button>{'Сохранить'}</button>
+        <button onClick={saveData}>{'Сохранить'}</button>
       </div>
     </div>
   );
